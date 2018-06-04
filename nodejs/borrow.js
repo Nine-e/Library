8f3e7765aff502bf24469e7431d93cfc6ed5b2a2
Nine-e <3287746498@qq.com>
2018-06-04 09:17:21 +0800

    5

--- nodejs/borrow.js
+++ nodejs/borrow.js
@@ -1,21 +1,28 @@
+var express = require('express');
+var app = express();
 var redis = require('redis'),
-    client = redis.createClient();
+	client = redis.createClient();
 
- var email = req.query.email;
- client.on('connect',function(){
-    client.hmset('1', {'js':'javascript','C#':'C Sharp'}, redis.print);
-    client.hmset('1', 'SQL','Structured Query Language','HTML','HyperText Mark-up Language', redis.print);
+app.get('/html/borrow', function(req, res) {
+	var email = req.query.email;
+	
+	client.on('connect', function() {
+		client.hmset('1', {
+			'js': 'javascript',
+			'C#': 'C Sharp'
+		}, redis.print);
+		client.hmset('1', 'SQL', 'Structured Query Language', 'HTML', 'HyperText Mark-up Language', redis.print);
 
-    client.hgetall(email, function(err,res){
-        if(err)
-        {
-            console.log('Error:'+ err);
-            return;
-        }            
-       console.log(res);
-    });
-});
+		client.hgetall(email, function(err, res) {
+			if (err) {
+				console.log('Error:' + err);
+				return;
+			}
+			console.log(res);
+		});
+	});
 
-client.on('ready',function(err){
-    console.log('ready');
-});
+	client.on('ready', function(err) {
+		console.log('ready');
+	});
+})
